b3f6401f08243d2fab8fb795f7c04758a05be5f4
Isaque-cyber <isaque_r_silva@estudante.sesisenai.org.br>
2026-04-09 20:57:52 -0300

    alterções para melhor funcionamento do código

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { prisma } from './prisma/prisma';
-import type { Exame, Usuario } from './prisma/generated/prisma/client';
+import { auth } from './middleware/auth';
+import cors from "cors"
+import { authRouter } from './routes/auth';
+import { usuarioRouter } from './routes/usuario';
+import { exameRouter } from './routes/exame';
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 const port = 3000;
 
 app.get('/', (req, res) => {
@@ -11,127 +15,13 @@ app.get('/', (req, res) => {
   res.send("Hello world")
 })
 
-// Endpoints usuario
-app.get('/usuarios', async (_, res) => {
-  const usuarios = await prisma.usuario.findMany();
-  return res.json(usuarios);
-})
+app.use(authRouter)
 
-app.get('/usuarios/:id', async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const usuario = await prisma.usuario.findUnique({
-    where: {
-      id: idUsuario
-    }
-  })
+//midleware de autenticação
+app.use(auth)
 
-  return res.status(200).json(usuario);
-})
-
-app.post("/usuarios", async (req, res) => {
-  console.log(req.body)
-  const dadosUsuario = req.body as Usuario
-  const usuarioCriado = await prisma.usuario.create({
-    data: {
-      email: dadosUsuario.email,
-      nome: dadosUsuario.nome || null,
-      senha: dadosUsuario.senha
-    }
-  })
-  return res.status(201).json(usuarioCriado)
-})
-
-app.put("/usuarios/:id", async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const dadosParaAtualizar = req.body as Omit<Usuario, 'id'>
-
-  const usuarioAtualizado = await prisma.usuario.update({
-    data: {
-      ...dadosParaAtualizar
-    },
-    where: {
-      id: idUsuario
-    }
-  })
-
-  return res.status(200).json(usuarioAtualizado);
-})
-
-app.delete('/usuarios/:id', async (req, res) => {
-  const idUsuario = Number(req.params.id)
-  const usuarioDeletado = await prisma.usuario.delete({
-    where: {
-      id: idUsuario
-    }
-  })
-
-  return res.status(200).json({
-    mensagem: "Usuário deletado com sucesso!",
-    data: usuarioDeletado
-  });
-})
-
-//Exames
-app.get('/exames', async (_, res) => {
-  const exames = await prisma.exame.findMany();
-  return res.json(exames);
-})
-
-app.get('/exames/:id', async (req, res) => {
-  const idExame = Number(req.params.id)
-  const exame = await prisma.exame.findUnique({
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json(exame);
-})
-
-app.post("/exames", async (req, res) => {
-  const dadosExame = req.body as Exame
-  const exameCriado = await prisma.exame.create({
-    data: {
-      tipo_exame: dadosExame.tipo_exame,
-      valor: dadosExame.valor,
-      descricao: dadosExame.descricao,
-      data_exame: new Date(dadosExame.data_exame),
-      resultado: dadosExame.resultado
-    }
-  })
-  return res.status(201).json(exameCriado)
-})
-
-app.put("/exames/:id", async (req, res) => {
-  const idExame = Number(req.params.id)
-  const dadosParaAtualizar = req.body as Omit<Exame, 'id'>
-
-  const exameAtualizado = await prisma.exame.update({
-    data: {
-      ...dadosParaAtualizar,
-      data_exame: new Date(dadosParaAtualizar.data_exame)
-    },
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json(exameAtualizado);
-})
-
-app.delete('/exames/:id', async (req, res) => {
-  const idExame = Number(req.params.id)
-  const exameDeletado = await prisma.exame.delete({
-    where: {
-      id: idExame
-    }
-  })
-
-  return res.status(200).json({
-    mensagem: "Exame deletado com sucesso!",
-    data: exameDeletado
-  });
-})
+app.use(usuarioRouter)
+app.use(exameRouter)
 
 app.listen(port, () => {
   console.log("Servidor ta de pé :p")
